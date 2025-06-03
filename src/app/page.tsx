5a96a6860d4fd597ea3f17a971fca2447da02017
Mateus Galvão de Camargo <mateus.galvao.camargo.contato@gmail.com>
2025-06-03 02:48:30 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (<div>ahã</div>);
+  return ( 
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
